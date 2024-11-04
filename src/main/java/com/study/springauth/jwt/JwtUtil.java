1ba793ff9a5e80de60170e18b02e95e9a719b647
jaehee-An <jhh9615@gmail.com>
2024-11-04 14:30:20 +0900

    filter

--- src/main/java/com/study/springauth/jwt/JwtUtil.java
+++ src/main/java/com/study/springauth/jwt/JwtUtil.java
@@ -1,6 +1,7 @@
 package com.study.springauth.jwt;
 
 import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 import java.net.URLEncoder;
 import java.security.Key;
 import java.util.Base64;
@@ -24,6 +25,7 @@ import io.jsonwebtoken.security.Keys;
 import io.jsonwebtoken.security.SignatureException;
 import jakarta.annotation.PostConstruct;
 import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 
@@ -125,4 +127,19 @@ public class JwtUtil {
 		return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody();
 	}
 
+	public String getTokenFromRequest(HttpServletRequest req) {
+		Cookie[] cookies = req.getCookies();
+		if(cookies != null) {
+			for (Cookie cookie : cookies) {
+				if (cookie.getName().equals(AUTHORIZATION_HEADER)) {
+					try {
+						return URLDecoder.decode(cookie.getValue(), "UTF-8"); // Encode 되어 넘어간 Value 다시 Decode
+					} catch (UnsupportedEncodingException e) {
+						return null;
+					}
+				}
+			}
+		}
+		return null;
+	}
 }
